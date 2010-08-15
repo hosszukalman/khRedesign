39b51c927bb0b80a530fa1d457d955e3f7c0c711
Kalman Hosszu <hosszu.kalman@gmail.com>
2010-08-15 13:57:48 +0200

    Fine left region blocks layout

--- themes/custom/kalmanhosszu/block.tpl.php
+++ themes/custom/kalmanhosszu/block.tpl.php
@@ -26,7 +26,7 @@
  * @see template_preprocess_block()
  */
 ?>
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?>">
+<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="block block-<?php print $block->module ?><?php if ($block_id == 1) echo ' first' ?>">
 <?php if ($block->subject): ?>
   <h2><?php print $block->subject ?></h2>
 <?php endif;?>
